d6a1d24576e82bf3fc6d05036ee372eff27fa2f0
Will Maier <wcmaier@gmail.com>
2018-01-28 13:27:28 -0800

    Update words

--- words.sh
+++ words.sh
@@ -5,5 +5,5 @@ echo "package main"
 echo "var Words = map[string]Pos {"
 curl -s https://raw.githubusercontent.com/whilp/words/master/wordlist.csv | sed -e 's/^\(.*\),\(.*\)$/	"\1": \2,/'
 echo "}"
-) > words.go
+) | gofmt -w words.go
 
